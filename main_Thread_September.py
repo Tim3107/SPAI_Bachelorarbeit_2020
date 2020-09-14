89d8f6cd00fba90cf2a252320109455256531eaa
Tim3107 <66574301+Tim3107@users.noreply.github.com>
2020-09-14 15:00:41 +0200

    Add files via upload

--- main_Thread_September.py
+++ main_Thread_September.py
@@ -11,6 +11,7 @@ from Thread_SPAI_September import myThread
 import time
 from load_data import load_data
 from scipy import sparse
+import sys
 
 def load_Matrix_A(level):
     Matrix_A = load_data(level)
@@ -22,12 +23,14 @@ def load_Matrix_A(level):
 
 if __name__ == "__main__":
     
-    epsilon = 0.1                                   #obere Schranke Abbruchkriterium Residuum ||r-Am||
-    max_fillin = 4                                  #obere Schranke an Einträgen in den Spalten aus M
-    case_Abbruchkriterium = 0                       #Die Art des Abbruchkriteriums... Kombiniert?
-    Anzahl_neue_Indizes_J_pro_Iteration = 2         #Anzahl an neuen Einträgen, die pro Iterationsschritt zu J hinzugefügt werden 
-
-    level = 1
+    epsilon = float(sys.argv[1])                     #obere Schranke Abbruchkriterium Residuum ||r-Am||
+    max_fillin = int(sys.argv[2])                   #obere Schranke an Einträgen in den Spalten aus M
+    case = int(sys.argv[3])                                 #Die Art des Abbruchkriteriums... Kombiniert?
+    Anzahl_neue_Indizes_J_pro_Iteration = int(sys.argv[4])      #Anzahl an neuen Einträgen, die pro Iterationsschritt zu J hinzugefügt werden 
+    level = int(sys.argv[5])
+    
+    print("Der Algorithmus wird gestartet mit den Parametern \n " "epsilon = ",epsilon,"\n max_fillin = ",max_fillin,"\n case = ",case,"\n level = ",level,"\n Anzahl_neue_Indizes_J_pro_Iteration = ",Anzahl_neue_Indizes_J_pro_Iteration)
+    
     [Matrix_A,M_Default,M,Dimension] = load_Matrix_A(level)
     Matrix_A = Matrix_A.tocsc()
     Matrix_A.eliminate_zeros()
@@ -36,7 +39,7 @@ if __name__ == "__main__":
     Database.getcol_boost()
     Threads = []
     for i in range(0,Dimension):
-        Threads.append(threading.Thread(target=myThread().Start, args=(epsilon,max_fillin,case_Abbruchkriterium,Anzahl_neue_Indizes_J_pro_Iteration,M,M_Default.getcol(i),Dimension,i,Database)))
+        Threads.append(threading.Thread(target=myThread().Start, args=(epsilon,max_fillin,case,Anzahl_neue_Indizes_J_pro_Iteration,M,M_Default.getcol(i),Dimension,i,Database)))
     l = len(Threads)
     time_start = time.time()
     for j in range(0,l):
